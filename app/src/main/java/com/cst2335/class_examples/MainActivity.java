8175062045813798ad5fcddbbf8bf685ab90bb36
Samira Ouaaz <souaaz@yahoo.com>
2022-01-19 21:21:49 -0500

    Initial commit

---
+++ app/src/main/java/com/cst2335/class_examples/MainActivity.java
@@ -0,0 +1,19 @@
+package com.cst2335.class_examples;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
